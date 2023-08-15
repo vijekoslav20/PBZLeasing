4c18f642e5325f92021f8448e409e2cdf47d9b8e
vijekoslav20 <137533220+vijekoslav20@users.noreply.github.com>
2023-08-15 11:47:11 +0200

    Add files via upload

--- js/navigationScript.js
+++ js/navigationScript.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-	$('nav ul li a:not(:only-child)').click(function(e) {
-		$(this).siblings('.nav-dropdown').slideToggle();
-		$('.nav-dropdown').not($(this).siblings()).hide();
-		e.stopPropagation();
-	});
+	// $('nav ul li a:not(:only-child)').click(function(e) {
+		// $(this).siblings('.nav-dropdown').slideToggle();
+		// $('.nav-dropdown').not($(this).siblings()).hide();
+		// e.stopPropagation();
+	// });
 
-	$('html').click(function(){
-		$('.nav-dropdown').hide();
-	})
+	// $('html').click(function(){
+		// $('.nav-dropdown').hide();
+	// })
 	
 	$('#nav-toggle').click(function(){
 		$('nav ul').slideToggle();
